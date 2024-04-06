595f3a8e1bd667a4632678c342cbfb026d4d84eb
akinori-nakayama <akinori.nakayama@play.jp>
2024-04-07 03:55:46 +0900

    ぼやき

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # TODO 複数形とか単数系とかキモいな
   resources :shared_texts, only: [:index, :create, :show]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
